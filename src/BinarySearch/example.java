1f35814b1b08d5199e44bbc49b1b63b4a1af2693
zhunyue <czy9608@gmail.com>
2019-09-29 20:25:17 -0700

    Add solution for binary search and sort examples

--- src/BinarySearch/example.java
+++ src/BinarySearch/example.java
@@ -1,4 +1,62 @@
 package BinarySearch;
 
 public class example {
+        //第一个大于某值
+        public static int Search1(int[] arr, int t){
+            int left=0;
+            int right=arr.length-1;
+            while(left<=right){
+                int mid=(left+right);
+                if(arr[mid]<=t){
+                    left=mid+1;
+                }
+                else
+                    right=mid-1;
+            }
+            if(left==arr.length)
+                return -1;
+
+            return left;
+        }
+
+        //最后一个小于等于
+        public static int Search2(int[] arr, int t){
+            int left=0;
+            int right=arr.length-1;
+            while(left<=right){
+                int mid=(left+right);
+                if(arr[mid]<=t){
+                    left=mid+1;
+                }
+                else
+                    right=mid-1;
+            }
+            if(left==arr.length)
+                return -1;
+            return left-1;
+        }
+
+        //最后一个等于
+        public static int Search3(int[] arr, int t){
+            int left=0;
+            int right=arr.length-1;
+            while(left<=right){
+                int mid=(left+right);
+                if(arr[mid]<=t){
+                    left=mid+1;
+                }
+                else
+                    right=mid-1;
+            }
+            if(right>=0&&arr[right]==t)
+                return right;
+            return -1;
+        }
+
+        public static void main(String[] args){
+            int[] arr={0,0,0,1,3,5,7,9};
+            System.out.println(Search1(arr,6));
+            System.out.println(Search2(arr,2));
+            System.out.println(Search3(arr,0));
+        }
 }
--- src/sort/sortExample.java
+++ src/sort/sortExample.java
@@ -1,4 +1,63 @@
 package sort;
 
 public class sortExample {
+    public static void quickSort(int[] a, int l, int r){
+        if(l>r)
+            return;
+
+        int i,j,index;
+        i=l;
+        j=r;
+        index=a[i];
+        while(i<j){
+            while(i<j&&a[j]>=index)
+                j--;
+            if(i<j)
+                a[i++]=a[j];
+            while(i<j&&a[i]<index)
+                i++;
+            if(i<j)
+                a[j--]=a[i];
+        }
+        a[i]=index;
+        quickSort(a,l,i-1);
+        quickSort(a,i+1,r);
+    }
+    public static void merge(int[] a, int l, int mid, int r){
+        int[] tmp=new int[r-l+1];
+        int i=l;
+        int j=mid+1;
+        int k=0;
+        while(i<=mid&&j<=r){
+            if(a[i]<a[j])
+                tmp[k++]=a[i++];
+            else
+                tmp[k++]=a[j++];
+        }
+        int index=(i<=mid)?i:j;
+        int end=(i<=mid)?mid:r;
+        while(index<=end){
+            tmp[k++]=a[index++];
+        }
+        for(int b=0;b<tmp.length;b++){
+            a[b+l]=tmp[b];
+        }
+    }
+    public static void mergeSort(int[] a, int l, int r){
+        int mid=(l+r)/2;
+        if(l<r){
+            mergeSort(a,l,mid);
+            mergeSort(a,mid+1,r);
+            merge(a,l,mid,r);
+        }
+
+    }
+    public static void main(String[] args){
+        int[] nums={1,2,5,3,9,4};
+        //quickSort(nums,0,nums.length-1);
+        mergeSort(nums,0,nums.length-1);
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
 }
