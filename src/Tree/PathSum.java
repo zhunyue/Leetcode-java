a44d8523a0b80718e9b4d907b5670c12ce545db0
zhunyue <czy9608@gmail.com>
2019-09-29 20:22:05 -0700

    Add solution for problem 112 and 113

--- src/Tree/PathSum.java
+++ src/Tree/PathSum.java
@@ -1,4 +1,47 @@
 package Tree;
 
+/*
+    Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+    Note: A leaf is a node with no children.
+
+    Example:
+
+    Given the below binary tree and sum = 22,
+
+          5
+         / \
+        4   8
+       /   / \
+      11  13  4
+     /  \      \
+    7    2      1
+    return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ */
 public class PathSum {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
+        return helper(root, sum, 0);
+    }
+
+    public boolean helper(TreeNode root, int sum, int cur){
+        if(root.left == null && root.right == null){
+            return (cur+root.val) == sum;
+        } else if (root.left != null && root.right != null){
+            return helper(root.left, sum, cur+root.val) || helper(root.right, sum, cur+root.val);
+        } else if(root.left == null){
+            return helper(root.right, sum, cur+root.val);
+        } else{
+            return helper(root.left, sum, cur+root.val);
+        }
+    }
 }
--- src/Tree/PathSum2.java
+++ src/Tree/PathSum2.java
@@ -1,4 +1,62 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+    Note: A leaf is a node with no children.
+
+    Example:
+
+    Given the below binary tree and sum = 22,
+
+          5
+         / \
+        4   8
+       /   / \
+      11  13  4
+     /  \    / \
+    7    2  5   1
+    Return:
+
+    [
+       [5,4,11,2],
+       [5,8,4,5]
+    ]
+ */
 public class PathSum2 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        if(root == null)
+            return res;
+        helper(root, sum, res, cur, 0);
+        return res;
+    }
+
+    public void helper(TreeNode root, int target, List<List<Integer>> res, List<Integer> cur, int curSum){
+        curSum += root.val;
+        cur.add(root.val);
+        if(root.left == null && root.right == null){
+            if(curSum == target){
+                res.add(new ArrayList<>(cur));
+            }
+        }
+        if(root.left != null){
+            helper(root.left, target, res, cur, curSum);
+            cur.remove(cur.size()-1);
+        }
+        if(root.right != null){
+            helper(root.right, target, res, cur, curSum);
+            cur.remove(cur.size()-1);
+        }
+    }
 }
