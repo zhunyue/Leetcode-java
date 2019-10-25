0bbcd69e7d17b335c8d8cc7cd81e124f4d88d971
zhunyue <czy9608@gmail.com>
2019-10-24 17:06:13 -0700

    Add solution for problem 142

--- src/LinkedList/LinkedListCycle2.java
+++ src/LinkedList/LinkedListCycle2.java
@@ -1,4 +1,49 @@
 package LinkedList;
 
+/*
+    Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+    To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+    Note: Do not modify the linked list.
+
+
+
+    Example 1:
+
+    Input: head = [3,2,0,-4], pos = 1
+    Output: tail connects to node index 1
+    Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ */
 public class LinkedListCycle2 {
+    /*
+        1. Use fast and slow pointer
+        2. If there is a cycle they must meet at somewhere
+        3. Two pointer from head and meet point respectively, they will meet at the point where the cycle begins
+     */
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+    }
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                ListNode slow2 = head;
+                while(slow2 != slow){
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
 }
