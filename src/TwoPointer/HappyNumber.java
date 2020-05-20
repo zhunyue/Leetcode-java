df36ab3f030208ed2c0e17b96121b020863e9813
zhunyue <czy9608@gmail.com>
2020-05-19 17:57:02 -0700

    Add solution

--- src/TwoPointer/HappyNumber.java
+++ src/TwoPointer/HappyNumber.java
@@ -1,4 +1,62 @@
 package TwoPointer;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Write an algorithm to determine if a number n is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Return True if n is a happy number, and False if not.
+
+Example:
+
+Input: 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+ */
 public class HappyNumber {
+    //General idea: doing calculation to see if we can reach a result of 1 or get into a loop
+    public int squareSum(int n){
+        int sum = 0;
+        while(n != 0){
+            int digit = n % 10;
+            sum += (digit*digit);
+            n /= 10;
+        }
+        return sum;
+    }
+
+    //Solution 1: use slow and fast pointer, if there is a number repeat, slow and fast pointer will meet
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = squareSum(slow);
+        while(fast != 1 && slow != fast){
+            slow = squareSum(slow);
+            fast = squareSum(squareSum(fast));
+        }
+        return fast == 1;
+    }
+
+    //solution 2: Keep a set to check if there is a loop happen (a number repeat)
+    public boolean isHappy2(int n) {
+        Set<Integer> set = new HashSet();
+        set.add(n);
+        while(true) {
+            n = squareSum(n);
+            if(n == 1)
+                return true;
+            else if(set.contains(n))
+                return false;
+            else{
+                set.add(n);
+            }
+        }
+    }
 }
--- src/TwoPointer/MoveZeroes.java
+++ src/TwoPointer/MoveZeroes.java
@@ -1,4 +1,43 @@
 package TwoPointer;
 
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+ */
 public class MoveZeroes {
+    //Solution1: Move all nonZero element to the front and fill the remaining with 0
+    //Time complexity: O(n)
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != 0)
+                nums[count++] = nums[i];
+        }
+        while (count < nums.length){
+            nums[count] = 0;
+            count++;
+        }
+    }
+
+    //Solution2: Similarly, using two pointers, one for iteration, one for keeping the last non zero position
+    // LastNonZero is the position that marks the last element that we recognized as non-zero
+    //Time complexity: O(n) . But if the input looks like [0,0,0,0,1], this one performs better than the one above
+    public void moveZeroes2(int[] nums) {
+        int lastNonZero = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int tmp = nums[lastNonZero++];
+                nums[lastNonZero - 1] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+    }
 }
